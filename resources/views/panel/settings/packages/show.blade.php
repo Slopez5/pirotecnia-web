b11c2d142e0d5b7312ea4fd681b512698d0d1b69
Sergio Lopez <sergio0695@gmail.com>
2024-07-25 21:36:44 -0600

    delete products modo cliente

--- resources/views/panel/settings/packages/show.blade.php
+++ resources/views/panel/settings/packages/show.blade.php
@@ -47,7 +47,6 @@
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <livewire:panel.settings.packages.show.product-groups-table :package="$package" />
                     <livewire:panel.settings.packages.show.products-table :package="$package" />
                 </div>
                 <div class="col-md-6">
