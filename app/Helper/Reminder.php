af1a2572ad8e5a99494890c7e502b1ec69499eed
Sergio Lopez <sergio0695@gmail.com>
2024-10-15 19:48:03 -0600

    fix bug create reminder

--- app/Helper/Reminder.php
+++ app/Helper/Reminder.php
@@ -4,17 +4,21 @@ namespace App\Helper;
 
 use App\Jobs\SendReminder;
 use App\Models\Event;
-use Illuminate\Support\Facades\Date;
 use Carbon\Carbon;
 
-class Reminder {
-    public static function send(Event $event, $method, $days, $sendToOwner = false) {
+class Reminder
+{
+    public static function send(Event $event, $method, $days, $sendToOwner = false)
+    {
         // Ejecuta el job para enviar el recordatorio
 
-        if (Date::parse($event->event_date)->diffInDays() <= $days) {
+        logger('Event date: ' . $event->event_date);
+        logger('Now: ' . now());
+        logger('Diff in days: ' . Carbon::parse($event->event_date)->diffInDays(now()));
+        if (Carbon::parse($event->event_date)->diffInDays(now()) <= $days) {
             SendReminder::dispatch($method, $event, $sendToOwner);
         } else {
-            SendReminder::dispatch($method, $event,$sendToOwner)->delay(Date::parse($event->event_date)->subDays($days));
+            SendReminder::dispatch($method, $event, $sendToOwner)->delay(Carbon::parse($event->event_date)->subDays($days));
         }
     }
 }
