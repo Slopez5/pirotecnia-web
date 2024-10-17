f677b48c7be9dfe9f6112d4c8aa8b8bc620cb8f1
Sergio Lopez <sergio0695@gmail.com>
2024-10-16 20:20:43 -0600

    fix reminder

--- app/Http/Controllers/Panel/EventController.php
+++ app/Http/Controllers/Panel/EventController.php
@@ -90,7 +90,6 @@ class EventController extends Controller
     {
         $event = Event::find($id);
         Reminder::send($event, 'whatsapp', 0, true);
-        sleep(20);
         Reminder::send($event, 'whatsapp', 0, false);
         return redirect()->route('events.show', $id);
     }
