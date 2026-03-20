373e941b3a185ea8016492e42857c93281a19ba4
Sergio Lopez <dev.sergio0695@gmail.com>
2026-03-20 14:43:13 -0600

    add logout controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -53,4 +53,13 @@ class AuthController extends Controller
 
         return redirect()->route('dashboard');
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
 }
